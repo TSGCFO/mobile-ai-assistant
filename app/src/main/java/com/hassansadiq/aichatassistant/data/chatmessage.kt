252617e4642375666679435ae1ea29f9ea0c56bc
Hassan Sadiq <h.sadiq@tsgfulfillment.com>
2025-09-26 10:44:28 -0400

    chore: add ChatMessage.kt from Dropbox Mobile-app

---
+++ app/src/main/java/com/hassansadiq/aichatassistant/data/chatmessage.kt
@@ -0,0 +1,8 @@
+package com.HassanSadiq.AIChatAssistant.data
+
+data class ChatMessage(
+    val id: String = "",
+    val content: String,
+    val isFromUser: Boolean,
+    val timestamp: Long = System.currentTimeMillis()
+)
